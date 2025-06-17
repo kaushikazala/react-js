57093c2c2f48991d935970646dc2db8ed5b04fda
kaushika zala <kaushikazala4@gmail>
2025-06-17 15:49:32 +0530

    counterApp

--- day3/react-app/src/App.jsx
+++ day3/react-app/src/App.jsx
@@ -1,23 +1,33 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  let [count,setData] = useState(0);
-  useEffect(()=>{
-  },[count])
-  return (
+  let [count, setCount] = useState( 
+    JSON.parse(localStorage.getItem("count")) || 0);
     
+      useEffect(() => {
+    localStorage.setItem("local", JSON.stringify(count));
+  }, [count]);
+  return(
     <>
       <h1>Count:{count}</h1>
-      <button onClick={()=>{
-        setData(count+1);
-      }}>inc</button>
-      <button onClick={()=>{
-        setData(count-1)
-      }}>dec</button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        inc
+      </button>
+      <button
+        disabled={count == 0}
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        dec
+      </button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </>
   )
-}
+    }
 
-
-export default App
+export default App;
